1e25625c1dda0709b5b13810ace53a74bd075aa5
Iswanul Umam <umam@sepulsa.com>
2018-10-05 18:44:52 +0700

    finish digit perkalian minimum

---
+++ 6-digit-perkalian-minimum.py
@@ -0,0 +1,20 @@
+def digitPerkalianMinimum(angka):
+  # you can only write your code here!
+  result = 0
+  minValue = 999
+  for i in range(1, angka + 1):
+    if angka % i == 0:
+      lengStr = str(i) + str(angka // i)
+      if len(lengStr) < minValue:
+        result = len(lengStr)
+        minValue = len(lengStr)
+  return result
+      
+
+
+# Driver Code
+print(digitPerkalianMinimum(24)) # 2
+print(digitPerkalianMinimum(90)) # 3
+print(digitPerkalianMinimum(20)) # 2
+print(digitPerkalianMinimum(179)) # 4
+print(digitPerkalianMinimum(1)) # 2
