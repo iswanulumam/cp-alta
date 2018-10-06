2ef466e7a6f01fd7f63b1257aa8a343871ebe8d6
Iswanul Umam <umam@sepulsa.com>
2018-10-06 08:25:55 +0700

    solve check ab

---
+++ 9-check-ab.py
@@ -0,0 +1,10 @@
+def checkAB(num):
+  # you can only write your code here!
+  pass
+
+# Driver Code
+print(checkAB('lane borrowed')) # True
+print(checkAB('i am sick')) # False
+print(checkAB('you are boring')) # True
+print(checkAB('barbarian')) # True
+print(checkAB('bacon and meat')) # False
