6f46ac75aba357d8c9ca43236c3b65cf92c1eee4
Iswanul Umam <iswanul.umam@gmail.com>
2018-12-06 00:17:51 +0700

    rework 3 lang

---
+++ 1-adhock/8-target-terdekat/solution.py
@@ -0,0 +1,9 @@
+def targetTerdekat(arr):
+  pass
+
+# Driver Code
+print(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])) # 3
+print(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])) # 4
+print(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])) # 1
+print(targetTerdekat([' ', ' ', 'o', ' '])) # 0
+print(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])) # 2
