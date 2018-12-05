e14ce0dab7866016fa68da5e94a910146ac3c355
Iswanul Umam <iswanul.umam@gmail.com>
2018-12-05 16:53:26 +0700

    naik angkot finish

---
+++ 1-adhock/7-naik-angkot.py
@@ -0,0 +1,24 @@
+def naikAngkot(arrPenumpang):
+  rute = ['A', 'B', 'C', 'D', 'E', 'F']
+
+  if len(arrPenumpang) == 0:
+    return []
+
+  result = []
+
+  for arr in arrPenumpang:
+    mapper = {}
+
+    mapper['penumpang'] = arr[0]
+    mapper['naikDari'] = arr[1]
+    mapper['tujuan'] = arr[2]
+    mapper['bayar'] = abs(rute.index(arr[1]) - rute.index(arr[2])) * 2000
+    result.append(mapper)
+
+  return result
+
+# Driver Code
+print(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]))
+# [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+#   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+print(naikAngkot([])) # []
