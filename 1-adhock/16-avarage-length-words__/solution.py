d2719b059180dcecf37f07a0440ee2b9ee4b07f5
Iswanul Umam <iswanul.umam@gmail.com>
2018-12-07 15:30:11 +0700

    new strategy

---
+++ 1-adhock/16-avarage-length-words__/solution.py
@@ -0,0 +1,6 @@
+def averageLengthWord(kata):
+  
+
+print(averageLengthWord('Do you want to become a great coder ?')) # ['you']
+print(averageLengthWord('You dont know what you have until you lose it!')) # [ 'dont','know','what','have','lose']
+print(averageLengthWord('I am diligent')) # []
