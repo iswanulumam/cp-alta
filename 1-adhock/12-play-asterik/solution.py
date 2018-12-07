ce112b98241155b3f0fac4b9b7d1c1395ca4373d
Iswanul Umam <iswanul.umam@gmail.com>
2018-12-07 12:44:30 +0700

    finish asterik_lima

--- 1-adhock/12-play-asterik/solution.py
+++ 1-adhock/12-play-asterik/solution.py
@@ -23,10 +23,65 @@ def asterik_dua(N):
 
 asterik_dua(5)
 '''
-* 
-* * 
-* * * 
-* * * * 
+    *
+   * *
+  * * *
+ * * * *
 * * * * *
 '''
+
+def asterik_tiga(N):
+  for i in range(1, N + 1):
+    list = []
+    for j in range(1, i + 1):
+      list.append(str(j))
+    print(" ".join(list))
+
+asterik_tiga(5)
+'''
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+'''
+
+def asterik_empat(N):
+  init = 1
+  for i in range(1, N + 1):
+    list = []
+    for j in range(1, i + 1):
+      list.append(str(init))
+      init += 1
+    print(" ".join(list))
+
+asterik_empat(5)
+'''
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+'''
+
+def asterik_lima(N):
+  for i in range(N - 1, -1, -1):
+    list = []
+    for j in range(1, N + 1):
+      if j > i:
+        list.append('*')
+      else:
+        list.append(' ')
+    print("".join(list))
+
+asterik_lima(5)
+'''
+    *
+   **
+  ***
+ ****
+*****
+'''
+
+
 
