8f3558d3ac4f42199395f26ce75f66b42decbfc4
Iswanul Umam <iswanul.umam@gmail.com>
2018-12-20 01:05:17 +0700

    finish roman rec

--- 15-hacktiv/4-roman-numeral-recursive/solution.go
+++ 15-hacktiv/4-roman-numeral-recursive/solution.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func toRoman(value int, index int) string {
 	var number = []int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
-	var roman = []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
+	roman := []string{"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"}
 	if index == -1 {
 		return ""
 	} else if value >= number[index] {
