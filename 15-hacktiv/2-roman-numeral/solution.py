e1beaa382fb833172ba09d0a458d2f5fa6905383
Iswanul Umam <iswanul.umam@gmail.com>
2018-12-19 14:57:14 +0700

    finish python roman numerals

--- 15-hacktiv/2-roman-numeral/solution.py
+++ 15-hacktiv/2-roman-numeral/solution.py
@@ -1,11 +1,17 @@
 def toRoman(value):
   roman = {
     1000: 'M',
+    900: 'CM',
     500: 'D',
+    400: 'CD',
     100: 'C',
+    90: 'XC',
     50: 'L',
+    40: 'XL',
     10: 'X',
+    9: 'IX',
     5: 'V',
+    4: 'IV',
     1: 'I',
   }
   result = ''
@@ -17,9 +23,9 @@ def toRoman(value):
 
 print('My totally sweet testing script\n')
 print('input | expected | actual')
-print('------------|--------------------|----------------')
-# print('4    | IV      | ', toRoman(4))
-# print('9    | IX      | ', toRoman(9))
-# print('23   | XXIII   | ', toRoman(23))
-# print('1453 | MCDLIII | ', toRoman(1453))
-# print('1646 | MDCXLVI | ', toRoman(1646))
+print('------|----------|--------')
+print('4     | IV       | ', toRoman(4))
+print('9     | IX       | ', toRoman(9))
+print('23    | XXIII    | ', toRoman(23))
+print('1453  | MCDLIII  | ', toRoman(1453))
+print('1646  | MDCXLVI  | ', toRoman(1646))
