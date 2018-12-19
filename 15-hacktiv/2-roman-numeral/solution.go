7e0d143934988a51aa1c1b7f08a5cd747274c5cf
Iswanul Umam <iswanul.umam@gmail.com>
2018-12-19 16:13:58 +0700

    roman numerals go

--- 15-hacktiv/2-roman-numeral/solution.go
+++ 15-hacktiv/2-roman-numeral/solution.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func toRoman(value int) string {
-	number := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	roman := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	var number = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	var roman = []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 
-	result := ""
+	var result string = ""
 	for i := 0; i < len(number); i++ {
 		for value >= number[i] {
 			result += roman[i]
