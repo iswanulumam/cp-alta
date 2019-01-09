59b040be671d36431bd08a8e746725a8b027fa7e
Iswanul Umam <iswanul.umam@gmail.com>
2019-01-10 00:20:01 +0700

    finish small positive

--- 1-adhock/20-small-positive-missing-number/solution.py
+++ 1-adhock/20-small-positive-missing-number/solution.py
@@ -1,5 +1,13 @@
 def smallPositiveMissing(array):
-  pass
+  number = [ False ] * 1000
+  number[0] = True
+  for a in array:
+    if a > 0:
+      number[a] = True
+  for n in range(1, len(number)):
+    if number[n] == False:
+      return n
+
 
 print(smallPositiveMissing([1, 2, 3, 4, 5])) # 6
 print(smallPositiveMissing([0, -10, 1, 3, -20])) # 2
