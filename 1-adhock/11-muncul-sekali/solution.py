f02d3dc5c74253074f2787fc6ee4fd17f3522719
Iswanul Umam <iswanul.umam@gmail.com>
2019-03-05 23:53:02 +0700

    finish muncul sekali

--- 1-adhock/11-muncul-sekali/solution.py
+++ 1-adhock/11-muncul-sekali/solution.py
@@ -1,4 +1,22 @@
 def munculSekali(angka):
-  pass
-print(munculSekali('1234123')) # [4]
-print(munculSekali('76523752')) # [6, 3]
+  map_angka = {}
+  result_angka = []
+  for a in angka:
+    key = a
+    if key not in map_angka:
+      map_angka[key] = 1
+    else:
+      map_angka[key] += 1
+  result = []
+  for a in angka:
+    key = a
+    if map_angka[key] == 1:
+      result.append(a)
+  return result
+
+print(munculSekali('1234123')) # ['4']
+print(munculSekali('76523752')) # ['6', '3']
+print(munculSekali('1122')) # []
+print(munculSekali('1122333')) # []
+print(munculSekali('112723335')) # ['7', '5']
+print(munculSekali('112523337')) # ['5', '7']
