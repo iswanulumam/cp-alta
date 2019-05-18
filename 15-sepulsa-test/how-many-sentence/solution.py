031a2d45d4f72d66c7e81eea6e72aef9940ece81
Iswanul Umam <iswanul.umam@gmail.com>
2019-05-18 12:48:37 +0700

    rework

--- 15-sepulsa-test/how-many-sentence/solution.py
+++ 15-sepulsa-test/how-many-sentence/solution.py
@@ -1,5 +1,19 @@
+def computeString(sentence, mapper):
+  count = 0
+  sentence_mapper = {}
+  return count  
+
 def countSentences(wordSet, sentences):
-  pass
+  result = []
+  mapper = {}
+  for word in wordSet:
+    key = word;
+    if key not in mapper:
+      mapper[''.join(sorted(key))] = True
+  for sentence in sentences:
+    count = computeString(sentence, mapper)
+    result.append(count)
+  return result
 
 wordSet = ['the', 'bats', 'tabs', 'in', 'cat', 'act']
 sentences = [
