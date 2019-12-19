0dd2c7996ba16619b3bc72bce346cdcc89756338
Iswanul Umam <iswanul.umam@gmail.com>
2019-12-19 13:39:42 +0700

    finish basic programming

--- go/1-basic/28-caesar-cipher.go
+++ go/1-basic/28-caesar-cipher.go
@@ -2,15 +2,67 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func caesarCipher(offset int, inputString string) string {
+// 'a' has code 97
+// 97 + ? (122-a = 25) = 122
 
+// 'b' has code 98
+// 'z' having code 122.
+
+func caesarCipher(offset int, inputString string) string {
+	var result string = ""
+	offset = offset % 26
+
+	if offset >= 0 {
+		for _, is := range inputString {
+			var (
+				intIS   int = int(is)
+				newChar int = intIS + offset
+			)
+			if string(is) == " " {
+				result += " "
+			} else if newChar > 122 {
+				result += string(newChar - 26)
+			} else {
+				result += string(newChar)
+			}
+		}
+	} else if offset < 0 {
+		for _, is := range inputString {
+			var (
+				intIS   int = int(is)
+				newChar int = intIS - int(math.Abs(float64(offset)))
+			)
+
+			if string(is) == " " {
+				result += " "
+			} else if newChar < 97 {
+				result += string(newChar + 26)
+			} else {
+				result += string(newChar)
+			}
+		}
+	}
+
+	return result
 }
 
 func main() {
-	fmt.Println(caesarCipher(3, "abc"))
-	// def
+
+	fmt.Println(caesarCipher(-1, "bcd"))
+	// abc
+	fmt.Println(caesarCipher(-1, "a"))
+	// z
+	fmt.Println(caesarCipher(-1000, "a"))
+	// o
+	fmt.Println(caesarCipher(-1000, "b"))
+	// p
+	fmt.Println(caesarCipher(-1, "abc"))
+	// zab
+	fmt.Println(caesarCipher(-1000, "abcdefghijklmnopqrstuvwxyz"))
+	// opqrstuvwxyzabcdefghijklmn
 	fmt.Println(caesarCipher(3, "abc xyz"))
 	// def abc
 	fmt.Println(caesarCipher(1, "abcdefghijklmnopqrstuvwxyz"))
@@ -25,11 +77,8 @@ func main() {
 	// fghijklmnopqrstuvwxyzabcde
 	fmt.Println(caesarCipher(26, "abcdefghijklmnopqrstuvwxyz"))
 	// abcdefghijklmnopqrstuvwxyz
-	// more test case
-	fmt.Println(caesarCipher(-1, "abc"))
-	// zab
-	fmt.Println(caesarCipher(-1000, "abcdefghijklmnopqrstuvwxyz"))
-	// opqrstuvwxyzabcdefghijklmn
 	fmt.Println(caesarCipher(1000, "abcdefghijklmnopqrstuvwxyz"))
 	// mnopqrstuvwxyzabcdefghijkl
+	fmt.Println(caesarCipher(1000000, "abcdefghijklmnopqrstuvwxyz"))
+	// opqrstuvwxyzabcdefghijklmn
 }
